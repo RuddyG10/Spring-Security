36aff4c24a10b0e592776ef6b7004c036d493cc4
RuddyG10 <rggt0001@ce.pucmm.edu.do>
2024-02-26 09:27:59 -0400

    Created the basic app

---
+++ src/main/java/com/example/springsecurity/config/MvcConfig.java
@@ -0,0 +1,21 @@
+package com.example.springsecurity.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    /**
+      View Controller
+     addViewControllers() - adds four view controller, two of them reference the view
+     whose name is home defined in home.html, and antoher to hello.html and login.
+     **/
+    public void addViewControllers(ViewControllerRegistry registry){
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+}
